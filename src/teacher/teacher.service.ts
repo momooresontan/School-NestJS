0bc217d4330ac27c353cac9e7bcb9316a57667f3
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-04-30 09:11:48 +0100

    teacher service delete update create get functions created

--- src/teacher/teacher.service.ts
+++ src/teacher/teacher.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 import { students, teachers } from '../db';
 import {
   FindTeacherResponseDto,
@@ -20,5 +21,48 @@ export class TeacherService {
     return this.teachers.find((teacher) => {
       teacher.id === teacherId;
     });
+  }
+
+  createTeacher(payload: CreateTeacherDto): TeacherResponseDto {
+    // eslint-disable-next-line prefer-const
+    let newTeacher = {
+      id: uuid,
+      ...payload,
+    };
+
+    this.teachers.push(newTeacher);
+
+    return newTeacher;
+  }
+
+  updateTeacher(payload: UpdateTeacherDto, teacherId: string) {
+    let updatedTeacher: TeacherResponseDto;
+
+    const updatedTeacherList = this.teachers.map((teacher) => {
+      if (teacher.id === teacherId) {
+        updatedTeacher = {
+          id: teacherId,
+          ...payload,
+        };
+      } else return teacher;
+    });
+
+    this.teachers = updatedTeacherList;
+
+    return updatedTeacher;
+  }
+
+  deleteTeacher(teacherId: string): FindTeacherResponseDto {
+    let removedTeacher;
+    const newTeacherList = this.teachers.map((teacher) => {
+      if (teacher.id === teacherId) {
+        const id = this.teachers.indexOf(teacher);
+        removedTeacher = this.teachers.splice(id, 1);
+      } else return teacher;
+    });
+
+    this.teachers = newTeacherList;
+
+    return removedTeacher;
   }
 }
