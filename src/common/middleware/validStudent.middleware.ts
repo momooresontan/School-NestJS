5c1c62136f07f178859392baf5bfa635bd699775
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-05-01 02:52:33 +0100

    valid student middleware implemented

--- src/common/middleware/validStudent.middleware.ts
+++ src/common/middleware/validStudent.middleware.ts
@@ -1,7 +1,18 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Injectable, NestMiddleware, HttpException } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { students } from '../../db';
 
 @Injectable()
 export class ValidStudentMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const studentId = req.params.id;
+    const studentExists = students.some((student) => {
+      student.id = studentId;
+    });
+    if (!studentExists) {
+      throw new HttpException('Student not found', 400);
+    }
+    next();
+  }
 }
